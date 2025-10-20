d9d2626f0367337f80749e73883c795d10081126
d4rkonus <d4rkonus@gmail.com>
2025-10-20 18:20:40 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -25,35 +25,43 @@ check_root_user(){
 # Update the system
 update_system(){
     echo -e "${blueColour}[+] Updating system...${endColour}"
-    apt update -y && apt upgrade -y 2>&1
+    apt update -y && apt upgrade -y
+    echo -e "${greenColour}[✓] System upgrades checked.${endColour}\n"
 }
-echo -e "${greenColour}[✓] System upgrades checked.${endColour}\n"
 
 # Install kitty
 install_kitty(){
     echo -e "${blueColour}[+] Installing Kitty and Zsh...${endColour}"
-    apt install -y kitty zsh 2>&1
+    apt install -y kitty zsh
+    echo -e "${greenColour}[✓] Kitty installed.${endColour}\n"
 }
-echo -e "${greenColour}[✓] Kitty installed.${endColour}\n"
-
 
 # Move the fonts
 move_fonts(){
-   echo -e "${blueColour}[+] Moving fonts...${endColour}"
-   cp -v "$ruta/fonts/"* /usr/share/fonts/ 2>&1
-   fc-cache -fv >/dev/null 2>&1
+    echo -e "${blueColour}[+] Moving fonts...${endColour}"
+
+    if [ -d "$ruta/fonts" ]; then
+        cp -v "$ruta/fonts/"* /usr/share/fonts/
+        fc-cache -fv >/dev/null 2>&1
+        echo -e "${greenColour}[✓] Fonts installed successfully.${endColour}\n"
+    else
+        echo -e "${redColour}[!] Fonts directory not found: $ruta/fonts${endColour}\n"
+    fi
 }
-echo -e "${greenColour}[✓] Fonts installed successfully.${endColour}\n"
 
 # Create kitty files
 create_kitty_files(){
     echo -e "${blueColour}[+] Creating kitty files...${endColour}"
-    mkdir -p /home/$USER/.config/kitty 2>&1
-    cp -vr "$ruta/config_kitty/"* /home/$USER/.config/kitty 2>&1
+
+    mkdir -p /home/$SUDO_USER/.config/kitty
+
+    if [ -d "$ruta/config_kitty" ]; then
+        cp -r "$ruta/config_kitty/"* /home/$SUDO_USER/.config/kitty/
+        echo -e "${greenColour}[✓] Kitty ready to run.${endColour}\n"
+    else
+        echo -e "${redColour}[!] Config directory not found: $ruta/config_kitty${endColour}\n"
+    fi
 }
-echo -e "${greenColour}[✓] Kitty really to road.${endColour}\n"
-
-
 
 # Execution
 #----------------------------------------------
