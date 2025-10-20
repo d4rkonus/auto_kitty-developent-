847c0f1b1e77a35d6b044b573d1c7165cc24061b
d4rkonus <d4rkonus@gmail.com>
2025-10-20 19:02:30 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -25,14 +25,14 @@ check_root_user(){
 # Update the system
 update_system(){
     echo -e "${blueColour}[+] Updating system...${endColour}"
-    apt update -y && apt upgrade -y
+    apt update -y >/dev/null 2>&1 && apt upgrade -y >/dev/null 2>&1
     echo -e "${greenColour}[✓] System upgrades checked.${endColour}\n"
 }
 
 # Install kitty
 install_kitty(){
     echo -e "${blueColour}[+] Installing Kitty and Zsh...${endColour}"
-    apt install -y kitty zsh
+    apt install -y kitty zsh >/dev/null 2>&1
     echo -e "${greenColour}[✓] Kitty installed.${endColour}\n"
 }
 
@@ -41,7 +41,7 @@ move_fonts(){
     echo -e "${blueColour}[+] Moving fonts...${endColour}"
 
     if [ -d "$ruta/fonts" ]; then
-        cp -v "$ruta/fonts/"* /usr/share/fonts/
+        cp -r "$ruta/fonts/"* /usr/share/fonts/ >/dev/null 2>&1
         fc-cache -fv >/dev/null 2>&1
         echo -e "${greenColour}[✓] Fonts installed successfully.${endColour}\n"
     else
@@ -53,15 +53,28 @@ move_fonts(){
 create_kitty_files(){
     echo -e "${blueColour}[+] Creating kitty files...${endColour}"
 
-    mkdir -p /home/$SUDO_USER/.config/kitty
+    mkdir -p /home/$USER/.config/kitty >/dev/null 2>&1
+    mkdir -p /root/.config >/dev/null 2>&1
 
     if [ -d "$ruta/config_kitty" ]; then
-        cp -r "$ruta/config_kitty/"* /home/$SUDO_USER/.config/kitty/
+        cp -r "$ruta/config_kitty/"* /home/$SUDO_USER/.config/kitty/ >/dev/null 2>&1
+        sudo cp -r "$ruta/config_kitty/"* /root/.config/ >/dev/null 2>&1
         echo -e "${greenColour}[✓] Kitty ready to run.${endColour}\n"
     else
         echo -e "${redColour}[!] Config directory not found: $ruta/config_kitty${endColour}\n"
     fi
 }
+
+
+# Install p10k
+p10k_install(){
+    echo -e "${blueColour}[+] Installing p10k...${endColour}"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.powerlevel10k
+    echo 'source ~/.powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+    sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /root/.powerlevel10k
+}
+
+
 
 # Execution
 #----------------------------------------------
